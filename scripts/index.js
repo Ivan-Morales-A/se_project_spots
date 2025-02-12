eb0d0b5c542d5dc7eb110e1e8842386e8e0a7226
Morales <Ivan.Morales@austintexas.gov>
2025-02-12 09:54:03 -0600

    Phase 1 Sprint 4

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+const initialCards = [
+{name: "Dark Alley", link: "https://images.unsplash.com/photo-1739192415587-f846fb6c731c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D"},
+{name: "Flower Bay", link: "https://images.unsplash.com/photo-1739361043380-f75ae825aee0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8"},
+{name: "Penguin Town", link: "https://images.unsplash.com/photo-1739056656210-7c3cab6fff42?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNnx8fGVufDB8fHx8fA%3D%3D"},
+{name: "Icy Mountain", link: "https://images.unsplash.com/photo-1739036462754-6e86520998a2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzMnx8fGVufDB8fHx8fA%3D%3D"},
+{name: "Secret Owl", link: "https://images.unsplash.com/photo-1734907865880-6eb669831b9e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0NHx8fGVufDB8fHx8fA%3D%3D"},
+{name: "US Flag", link: "https://plus.unsplash.com/premium_photo-1738779001501-8a84fd12a449?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyNXx8fGVufDB8fHx8fA%3D%3D"},
+];
+console.log(initialCards);
